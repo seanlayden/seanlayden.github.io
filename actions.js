74a3a62a5086fee4582096a997d707e161e36bdf
Sean Layden <seanlayden@gmail.com>
2014-09-13 13:31:15 -0600

    updating styles

--- actions.js
+++ actions.js
@@ -2,7 +2,20 @@ $(document).ready(function() {
 
   $(".box").on('click', 'button', function() {
     $(this).closest(".box").find('.seemore').slideToggle("fast");
+  });
+
+  $(".navbar-toggle").on('mouseenter', function() {
+    $(this).css("background-color", "white");
+    $(this).css("border", "1px solid #e8e8e8");
+  });
+
+  $(".navbar-toggle").on("mouseleave", function() {
+    $(this).css("background-color", "white");
+    $(this).css("border", "1px solid white");
   })
 
+  $(".navbar-toggle").on('click', function() {
+    $(this).css("background-color", "white");
+  })
 
 })
